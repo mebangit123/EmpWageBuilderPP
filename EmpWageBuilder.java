3074d1ac6a1554a1abbad49868e389d3523a0df1
mebangit123 <mebannongrum333@gmail.com>
2021-04-15 17:22:42 +0530

    Added logic to check Employee absent or present

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,13 @@
 public class EmpWageBuilder {
 public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+
+	int is_Present = 1;
+	double empCheck = Math.floor(Math.random() * 10) % 2;
+
+	if(empCheck == is_Present)
+		System.out.println("Employee is Present");
+	else
+		System.out.println("Employee is Absent");
 	}
 }
