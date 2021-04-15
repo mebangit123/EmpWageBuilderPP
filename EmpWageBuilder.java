72894eaf25aea879bdd5dc6bebae05cf8242cfd3
mebangit123 <mebannongrum333@gmail.com>
2021-04-15 18:03:18 +0530

    Calculating Wages for a month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,24 +2,31 @@ public class EmpWageBuilder {
 	public static final int is_FullTime = 1;
 	public static final int is_PartTime = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
+
 public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 
-		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-		switch(empCheck) {
-			case is_FullTime:
-				empHrs = 8;
-				break;
-			case is_PartTime:
-				empHrs = 4;
-				break;
-			default:
-			empHrs = 0;
+		for(int day=0; day<NUM_OF_WORKING_DAYS; day++) {
+			int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+			switch(empCheck) {
+				case is_FullTime:
+					empHrs = 8;
+					break;
+				case is_PartTime:
+					empHrs = 4;
+					break;
+				default:
+				empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Emp Wage: " + empWage);
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
