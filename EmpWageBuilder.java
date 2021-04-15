12099769991b25d2e461be7c3ba05744bcc332cd
mebangit123 <mebannongrum333@gmail.com>
2021-04-15 17:41:48 +0530

    Logic to add part employee added to main branch

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,15 +3,18 @@ public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 
 		int is_FullTime = 1;
+		int is_PartTime = 2;
 		int EMP_RATE_PER_HOUR = 20;
 
 		int empHrs = 0;
 		int empWage = 0;
 
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 
 		if(empCheck == is_FullTime)
 			empHrs = 8;
+		else if(empCheck == is_PartTime)
+			empHrs = 4;
 		else
 			empHrs = 0;
 
