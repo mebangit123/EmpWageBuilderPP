56dd9b5d6da899716c63313613246b0ce237f38c
mebangit123 <mebannongrum333@gmail.com>
2021-04-15 17:33:22 +0530

    Calculate daily Employee Wag added to main branch

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,12 +2,20 @@ public class EmpWageBuilder {
 public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 
-	int is_Present = 1;
-	double empCheck = Math.floor(Math.random() * 10) % 2;
+		int is_FullTime = 1;
+		int EMP_RATE_PER_HOUR = 20;
 
-	if(empCheck == is_Present)
-		System.out.println("Employee is Present");
-	else
-		System.out.println("Employee is Absent");
+		int empHrs = 0;
+		int empWage = 0;
+
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+
+		if(empCheck == is_FullTime)
+			empHrs = 8;
+		else
+			empHrs = 0;
+
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 }
