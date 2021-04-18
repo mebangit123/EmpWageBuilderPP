bfcc1fb367a40b37753fc0a5d514333130d8fce2
mebangit123 <mebannongrum333@gmail.com>
2021-04-18 22:18:20 +0530

    UC7 added to main branch

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,18 +1,18 @@
 public class EmpWageBuilder {
 	public static final int is_FullTime = 1;
 	public static final int is_PartTime = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HRS_IN_MONTH = 10;
+	//public static final int EMP_RATE_PER_HOUR = 20;
+	//public static final int NUM_OF_WORKING_DAYS = 2;
+	//public static final int MAX_HRS_IN_MONTH = 10;
 
-public static void main(String[] args) {
-	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+public static void calcEmpWage(int empRatePerHr, int numOfDays, int maxHourInMonth) {
 
-		int empHrs = 0;
-		int totalWorkingDays = 0;
-		int totalEmpHrs = 0;
+	int empHrs = 0;
+	int totalWorkingDays = 0;
+	int totalEmpHrs = 0;
+	int totalEmpWage = 0;
 
-		while( totalEmpHrs < MAX_HRS_IN_MONTH & totalWorkingDays < NUM_OF_WORKING_DAYS) {
+	while( totalEmpHrs < maxHourInMonth & totalWorkingDays < numOfDays) {
 			totalWorkingDays++;
 			int empCheck = (int)Math.floor(Math.random() * 10) % 3;
 			switch(empCheck) {
@@ -27,8 +27,15 @@ public static void main(String[] args) {
 			}
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: " + totalWorkingDays + " Emp Hour: " + empHrs);
-		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+	}
+		System.out.println("Total Emp Hours :" + totalEmpHrs);
+		totalEmpWage = totalEmpHrs * empRatePerHr;
+		System.out.println("Total Emp Wage :" + totalEmpWage);
+}
+
+public static void main(String[] args) {
+	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+
+		calcEmpWage(20,2,10);
 	}
 }
